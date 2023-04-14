2a54ea1da87c089b738bf0959f4a92f28ddc9bba
Vasil Sokolov <vasil@afterlogic.com>
2023-04-14 12:13:16 +0200

    Added Decorator method

--- Module.php
+++ Module.php
@@ -48,6 +48,15 @@ class Module extends \Aurora\System\Module\AbstractModule
         $this->subscribeEvent('Contacts::GetContactSuggestions', array($this, 'onGetContactSuggestions'));
     }
 
+    /**
+     *
+     * @return Module
+     */
+    public static function Decorator()
+    {
+        return parent::Decorator();
+    }
+
     public function onGetStorages(&$aStorages)
     {
         $aStorages[self::$iStorageOrder] = StorageType::Team;
